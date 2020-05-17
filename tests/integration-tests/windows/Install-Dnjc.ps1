13ed5af7fc93b1a20f1c6c7970b19017fde25b67
Christopher Warrington <code@cw.codes>
2020-05-17 04:03:08 -0700

    Add Windows integration tests
    Integration tests are driven by the TSV file
    tests/integration/test-data/test-list.tsv. Each line of this file is a
    test case. The fields are:
    
    * expected exit code
    * dnjc arguments
    * input file path, relative to test-list.tsv
    
    The unit tests have been moved from test/ to tests/unit-tests.
    
    Stub Unixy integration tests have been added as well.

---
+++ tests/integration-tests/windows/Install-Dnjc.ps1
@@ -0,0 +1,76 @@
+﻿<#
+.SYNOPSIS
+
+Installs the dnjc tool from a local package directory
+
+.OUTPUTS
+
+A PathInfo object for the installed executable
+
+#>
+[CmdletBinding(SupportsShouldProcess=$True)]
+param
+(
+    # Package directory.
+    #
+    # If not set, uses the pack\ directory in the repo root.
+    [string]
+    $SourcePath = $null,
+
+    # The version to install.
+    #
+    # If not set, will skip passing a version to `dotnet tool install`.
+    [string]
+    $Version = $null,
+
+    # Path to install dnjc to.
+    #
+    # If not set, uses a temporary directory.
+    [string]
+    $DestPath = $null
+)
+
+Set-StrictMode -Version Latest
+
+if (-not $SourcePath) {
+    $SourcePath = "$PSScriptRoot\..\..\..\pack\Debug\netcoreapp3.1\"
+}
+
+$SourcePath = Resolve-Path $SourcePath
+
+if (-not (Test-Path -LiteralPath $SourcePath -PathType Container)) {
+    throw "Source `"$SourcePath`" does not exist."
+}
+
+if (-not (Test-Path $DestPath -PathType Container)) {
+    $DestPath = [System.IO.Path]::Combine(
+        [System.IO.Path]::GetTempPath(),
+        [System.IO.Path]::GetRandomFileName())
+}
+
+if ($PSCmdlet.ShouldProcess('dnjc', "Install from `"$SourcePath`"")) {
+    $dotnetArgs = @(
+        'tool', 'install',
+        '--add-source', $SourcePath,
+        '--tool-path', $DestPath
+    )
+
+    if ($Version) {
+        $dotnetArgs += ('--version', $Version)
+    }
+
+    $dotnetArgs += ('DotNetJsonCheck.Tool')
+
+    & dotnet $dotnetArgs | Out-Null
+    if (-not $?) {
+        throw "dotnet tool install failed with $LASTEXITCODE"
+    }
+
+    $finalExecutable = Resolve-Path "$DestPath\dnjc.exe"
+
+    if (-not (Test-Path -LiteralPath $finalExecutable -PathType Leaf)) {
+        throw "Cannot find expected output `"$finalExecutable`""
+    }
+
+    Write-Output $finalExecutable
+}
---
+++ tests/integration-tests/windows/Test-Dnjc.ps1
@@ -0,0 +1,91 @@
+﻿<#
+.SYNOPSIS
+
+Runs integration tests against the given dnjc executable.
+
+.OUTPUTS
+
+The result of each test is written to either the output stream or the error
+stream, depending on whether it passed or not.
+
+The shape of the result object is:
+
+* ActualExitCode
+* Args
+* ExpectedExitCode
+* InputFile
+
+#>
+[CmdletBinding(SupportsShouldProcess=$True)]
+param
+(
+    # Path to the dnjc executable to test
+    [Parameter(Mandatory)]
+    [string]
+    $DnjcPath,
+
+    # Path to test-data folder. The file $TestDataPath\test-list.tsv must
+    # exist.
+    #
+    # The input files in test-list.tsv are assumed to be relative to
+    # $TestDataPath.
+    #
+    # If not set, defaults to ..\test-data
+    [string]
+    $TestDataPath = $null
+)
+
+Set-StrictMode -Version Latest
+
+
+if (-not $TestDataPath) {
+    $TestDataPath = "$PSScriptRoot\..\test-data\"
+}
+
+$testListFile = Join-Path $TestDataPath 'test-list.tsv'
+
+
+if (-not (Test-Path -LiteralPath $DnjcPath -PathType Leaf)) {
+    throw "Executable `"$DnjcPath`" does not exist."
+}
+
+if (-not (Test-Path -LiteralPath $testListFile -PathType Leaf)) {
+    throw "Test list `"$testListFile`" does not exist."
+}
+
+$importCsvArgs = @{
+    '-Delimiter' = "`t";
+    '-Encoding' = 'ASCII';
+    '-Header' = ('ExitCode','Args','InputFile');
+    '-LiteralPath' = $testListFile;
+}
+
+$testCases = Import-Csv @importCsvArgs
+
+$anyErrors = $False
+
+foreach ($testCase in $testCases) {
+    $dnjcArgs = $testCase.Args -split ' '
+    $inputFilePath = Join-Path $TestDataPath $testCase.InputFile
+    Get-Content -Encoding Ascii $inputFilePath | & $DnjcPath $dnjcArgs | Out-Null
+
+    $result = [PSCustomObject]@{
+        'ActualExitCode' = $LASTEXITCODE;
+        'Args' = $dnjcArgs -join ' ';
+        'ExpectedExitCode' = $testCase.ExitCode;
+        'InputFile' = $testCase.InputFile;
+    }
+
+    if ($LASTEXITCODE -eq $testCase.ExitCode) {
+        Write-Output $result
+    } else {
+        $anyErrors = $True
+        Write-Error $result
+    }
+}
+
+if ($anyErrors) {
+    exit 1
+} else {
+    exit 0
+}
